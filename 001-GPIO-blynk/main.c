79ad57cc4162ce5efe2860f28895081335ad16a9
Nguyen Tu Chung <95293469+NguyenTuChung01@users.noreply.github.com>
2022-11-08 22:37:58 +0700

    update

--- 001-GPIO-blynk/main.c
+++ 001-GPIO-blynk/main.c
@@ -1,21 +1,30 @@
 #include "stm32f10x.h"                  // Device header
 
+void delay(void);
+
+
 int main(void)
 {
+	SystemInit();
 	/* enable clock at port C*/
-	RCC->APB2ENR |= (1 << 4); 
+	RCC->APB2ENR |= (0x1 << 4); 
 	/* configure Pin3 on GPIOC*/
-	GPIOC->CRH |= (3 << 20); 		/*Output mode, max speed 50 MHz*/
-	GPIOC->CRH &= ~(3 << 22);	 	/*General purpose output push-pull*/
-	
-	GPIOC->BSRR |= (1 << 13);
+	GPIOC->CRH &= ~(0xF << 20);		/*Reset pin PC13*/
+	GPIOC->CRH |= (0x3 << 20); 		/*Set output push-pull*/ 
+	GPIOC->ODR |= (0x1 << 13);		/**/
 	while(1)
 	{
-		GPIOC->ODR |= (1 << 13);
-		for(uint32_t i = 0 ; i <300000; i++);
-		GPIOC->ODR &= ~(1 << 13);
-		for(uint32_t i = 0 ; i <300000; i++);
-
+		GPIOC->ODR &= ~(0x1 << 13);
+		delay();
+		GPIOC->ODR |= (0x1 << 13);
+		delay();
 	}
 	return 0;
 }
+
+void delay()
+{
+	uint32_t i;
+	for(i = 0 ; i < 100000 ; i++);
+}
+
