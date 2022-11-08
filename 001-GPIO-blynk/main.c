b9d3987de77a7d8a2fa4b15be996f09737de48a7
Nguyen Tu Chung <95293469+NguyenTuChung01@users.noreply.github.com>
2022-11-08 15:33:54 +0700

    change

--- 001-GPIO-blynk/main.c
+++ 001-GPIO-blynk/main.c
@@ -5,15 +5,16 @@ int main(void)
 	/* enable clock at port C*/
 	RCC->APB2ENR |= (1 << 4); 
 	/* configure Pin3 on GPIOC*/
-	GPIOC->CRH |= ( (1 << 20) | (1 << 21) ); 		/*Output mode, max speed 50 MHz*/
-	GPIOC->CRH &= ~( (1 << 22) | (1 << 23) );	 	/*General purpose output push-pull*/
+	GPIOC->CRH |= (3 << 20); 		/*Output mode, max speed 50 MHz*/
+	GPIOC->CRH &= ~(3 << 22);	 	/*General purpose output push-pull*/
+	
+	GPIOC->BSRR |= (1 << 13);
 	while(1)
 	{
-		int i;
-		GPIOC->BSRR |= (1 << 13);
-		for(i = 0; i <10000; i++);
-		GPIOC->BSRR &= ~(1 << 13);
-		for(i = 0; i <= 10000; i++);
+		GPIOC->ODR |= (1 << 13);
+		for(uint32_t i = 0 ; i <300000; i++);
+		GPIOC->ODR &= ~(1 << 13);
+		for(uint32_t i = 0 ; i <300000; i++);
 
 	}
 	return 0;
